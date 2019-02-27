6a4a8180873fe3e9d408cbb1c31b2f99bb78d470
Sanyyouisf <sanymamdoh@yahoo.com>
2019-02-27 10:23:45 -0600

    add emp type2

--- Solid/Program.cs
+++ Solid/Program.cs
@@ -12,7 +12,9 @@ namespace Solid
         {
             // to display the bonus
             Employee EmpSany = new Employee(1, "Sany", "permanent");
-            Console.WriteLine(string.Format("{0} - {1} - Bounus {2}" ,EmpSany.ToString(),EmpSany.EmpType ,EmpSany.CalculateBonus(50000).ToString()));
+            Employee EmpJhon = new Employee(2, "John", "temp");
+            Console.WriteLine(string.Format("{0} - {1} - Bounus {2}" , EmpSany.ToString(), EmpSany.EmpType, EmpSany.CalculateBonus(50000).ToString()));
+            Console.WriteLine(string.Format("{0} - {1} - Bounus {2}" , EmpJhon.ToString(), EmpJhon.EmpType, EmpJhon.CalculateBonus(150000).ToString()));
 
 
             Console.ReadLine();
